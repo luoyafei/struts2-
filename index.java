a55c69a4c5e68ccecad6d2ea76212bd0ccda309a
luoyafei <luoyafei.newinstance@foxmail.com>
2016-07-18 14:20:44 +0800

    accomplished a helf index.java

--- index.java
+++ index.java
@@ -96,7 +96,70 @@ chain/redirectAction：action上的转发和跳转；
 DomainModel必须提供一个参数为空的构造方法，用以struts来实例化一个DomainModel对象。否则会报错，
 struts无法确定该new哪个构造方法。
 
+【20】
+使用OGNL访问action中的一般方法，直接在<s:property value="m()">//这样可以访问action中的一般方法。
+使用OGNL访问action中DomainModel成员变量中的方法，<s:property value="user.add()">//使用成员变量加.来访问。
+使用OGNL访问类中的静态方法以及静态属性例子：<s:property value="@类名带包名@方法名或属性名" />。
+注意：访问类中的静态方法时，<constant name="struts.ognl.allowStaticMethodAccess" value="true" />
+必须将该项目加上，且设置为true。
+使用OGNL访问普通类的构造方法：<s:property value="new com.learning.bean.User(8)" />//打印出来的是User的toString()方法
+【21】使用OGNL访问List，Set，Map：
+在action中：
+	List<Cattle> cattles = new ArrayList<Cattle>();
+	Set<Cat> cats = new HashSet<Cat>();
+	Map<String, Dog> dogs = new HashMap<String, Dog>();
+	
+	public List<Cattle> getCattles() {
+		return cattles;
+	}
 
+	public void setCattles(List<Cattle> cattles) {
+		this.cattles = cattles;
+	}
+
+	public Set<Cat> getCats() {
+		return cats;
+	}
+
+	public void setCats(Set<Cat> cats) {
+		this.cats = cats;
+	}
+
+	public Map<String, Dog> getDogs() {
+		return dogs;
+	}
+
+	public void setDogs(Map<String, Dog> dogs) {
+		this.dogs = dogs;
+	}
+	
+	
+	public MyAction() {
+		cattles.add(new Cattle("cattle1", 1));
+		cattles.add(new Cattle("cattle2", 2));
+		cattles.add(new Cattle("cattle3", 3));
+		
+		cats.add(new Cat("cat1", 1));
+		cats.add(new Cat("cat2", 2));
+		cats.add(new Cat("cat3", 3));
+		
+		dogs.put("dog1", new Dog("dog1", 1));
+		dogs.put("dog2", new Dog("dog2", 2));
+		dogs.put("dog3", new Dog("dog3", 3));
+	}
+访问List：<s:property value="users" />//将该User的List全部打印出来。
+访问List中某个元素：<s:property value="users[1]">//users.get(0)
+访问List中元素某个属性的集合：<s:property value="users.{age}" />
+访问List中元素某个属性的集合中的特定值：
+<s:property value="users.{age}[0]" />或者<s:property value="users[0].age" />//推荐使用后者
+
+访问Set：<s:property value="dogs" />
+访问Set中某个元素：<s:property value="dogs[1]" />
+访问Map：<s:property value="dogMap" />
+访问Map中某个元素：<s:property value="dogMap.dog101" />//访问Map中key为dog101的value
+访问Map中所有的key：<s:property value="dogMap.keys" />
+访问Map中所有的value：<s:property value="dogMap.values" />
+访问容器的大小：<s:property value="dobMap.size()" />
 【作业】
 1,读doc文档:struts-tags;
 2,设计约定(编码规定)
